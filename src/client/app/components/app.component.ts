082838897d799d78bdf531b3833cccf53764a0a2
Hank Ehly <hank.ehly@mulodo.com>
2016-05-06 08:30:39 -0500

    Setup app-component

---
+++ src/client/app/components/app.component.ts
@@ -0,0 +1,18 @@
+/**
+ * app.component
+ * chat-app
+ *
+ * Created by henryehly on 5/6/16.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'ch-app',
+  templateUrl: 'app/components/app.component.html',
+  styleUrls: ['app/components/app.component.css']
+})
+
+export class AppComponent {
+
+}
--- src/client/main.ts
+++ src/client/main.ts
@@ -2,7 +2,7 @@ import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {APP_BASE_HREF} from 'angular2/platform/common';
-import {AppComponent} from 'app/components/app.component';
+import {AppComponent} from './app/components/app.component';
 
 if ('<%= ENV %>' === 'prod') { enableProdMode(); }
 
